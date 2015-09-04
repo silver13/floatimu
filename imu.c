5dc66ce329911bdfe942cc9ef20cdf4df82d3afc
silver13 <me@aaa.com>
2015-09-05 07:34:04 +0930

    v 0.02
    some optimizations

--- imu.c
+++ imu.c
@@ -35,6 +35,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 extern globalstruct global;
 extern usersettingsstruct usersettings;
 
+
+
+//#define SMALL_ANGLE_APPROX
+#ifdef SMALL_ANGLE_APPROX
+// Small angle approximation rotation 
+// with simple sin and cos
+#define ssin(val) (val)
+#define scos(val) 1.0f
+
+
+#else
+// version without small angle approximations defines for sin and cos
+// SMALL_ANGLE_APPROX not defined
+
+// proper rotation matrix with float sinf and cosf
+//#define _sinf(val) sinf(val)
+//#define _cosf(val) cosf(val)
+
+// proper rotation matrix with approximations for sin and cos
+// smaller size then above version
+#define _sinf(val) (val)
+#define _cosf(val)  (1 - ((val)*(val))/2) 
+#endif
+
+// gravity 1G size from accelerometer
+// should be smaller then 3000
+// this is the same as the int size
+
+#define ACC_1G 1172.0f
+
+
 // read the acc and gyro a bunch of times and get an average of how far off they are.
 // assumes the aircraft is sitting level and still.
 // If both==false, only gyro is calibrated and accelerometer calibration not touched.
@@ -69,7 +100,7 @@ void calibrategyroandaccelerometer(bool both)
         readgyro();
         if(both) {
             readacc();
-            //global.acc_g_vector[ZINDEX] -= FIXEDPOINTONE; // vertical vector should be at 1g
+            global.acc_g_vector[ZINDEX] -= FIXEDPOINTONE; // vertical vector should be at 1g
         }
 
         calculatetimesliver();
@@ -100,7 +131,7 @@ void calibrategyroandaccelerometer(bool both)
     }
 }
 
-static float GEstG[3] = {0 , 0 , 1024};
+static float GEstG[3] = {0 , 0 , ACC_1G};
 
 void initimu(void)
 {
@@ -122,50 +153,14 @@ void initimu(void)
 	readacc();
 	for ( int i = 0 ; i < 2;i++)
 	{
-	if ( i !=2 ) global.acc_g_vector[i] = global.acc_g_vector[i] + usersettings.acccalibration[i];
+	global.acc_g_vector[i] = global.acc_g_vector[i] + usersettings.acccalibration[i];
   GEstG[i] = global.acc_g_vector[i]>>6;
 	}
+	
 //lastbarorawaltitude = global.altitude = global.barorawaltitude;
-//    global.altitudevelocity = 0;
+//global.altitudevelocity = 0;
 
 }
-
-
-//#define SMALL_ANGLE_APPROX
-#ifdef SMALL_ANGLE_APPROX
-// Small angle approximation rotation 
-// with simple sin and cos
-#define ssin(val) (val)
-#define scos(val) 1.0f
-
-// Small angle approximation proper sin and 1 for cos
-// seems same as above
-//#define ssin(val) sinf(val)
-//#define scos(val) 1.0f
-
-// Small angle approximation with proper sin and cos
-// worse then all ( due to missing terms )
-//#define ssin(val) sinf(val)
-//#define scos(val) cosf(val)
-#else
-// version without small angle approximations defines for sin and cos
-// SMALL_ANGLE_APPROX not defined
-
-// proper rotation matrix with float sinf and cosf
-#define _sinf(val) sinf(val)
-#define _cosf(val) cosf(val)
-
-// proper rotation matrix with approximations for sin and cos
-// smaller size then above version
-//#define _sinf(val) (val)
-//#define _cosf(val)  (1 - ((val)*(val))/2) 
-#endif
-
-// gravity 1G size from accelerometer
-// should be smaller then 3000
-// this is the same as the int size
-
-#define ACC_1G 1172.0f
 
 
 void vectorcopy( float *vector1 , float *vector2);
@@ -173,21 +168,9 @@ void vectorcopy( float *vector1 , float *vector2);
 
 static unsigned long gp_timer ;
 
-/*
-static float debug1;
-static float debug2;	//
-static float debug3;	//
-static float debug4;	//
-static float debug5;
-static float debug6;
-static float debug7;	//
-static float debug8;
-static float debug9;
-*/
-
 float tofloat( fixedpointnum fp)
 {
-  float ans =  fp / 65536.0;
+  float ans =  fp * 1.5258789e-5; // (1/65536)
   return ans;
 }
 
@@ -198,7 +181,7 @@ float calcmagnitude( float  vector[3] )
  for (uint8_t axis = 0; axis < 3; axis++) {
 		accmag  +=  vector[axis] * vector[axis];
 	}	
-	accmag = sqrt ( accmag  );
+	accmag = sqrtf ( accmag  );
 	return accmag;
 }
 
@@ -216,14 +199,11 @@ void imucalculateestimatedattitude(void)
   float EstG[3];	
 	float acc[3]; // holds float accel vector
 	float deltatime; // time in seconds
-	 	 
-   const float normal = ACC_1G;
-	 
+	float gyros[3];
+	
 vectorcopy ( &EstG[0] , &GEstG[0] );
 	 
-	 deltatime = (float)lib_timers_gettimermicroseconds(gp_timer) * 0.000001f ; // time in seconds
-	 gp_timer = lib_timers_starttimer();
-//	 debug1 = deltatime;
+	 deltatime = (float)lib_timers_gettimermicrosecondsandreset(&gp_timer) * 0.000001f ; // time in seconds
 	 
 	 deltatime = deltatime* 0.92; // correction factor
 																// unknown reason 
@@ -234,73 +214,72 @@ vectorcopy ( &EstG[0] , &GEstG[0] );
 	 // x & y accel offsets only
     for ( int x = 0; x < 3; ++x) { // was 3
         global.gyrorate[x] = global.gyrorate[x] + usersettings.gyrocalibration[x];
-        if ( x !=2 ) global.acc_g_vector[x] = global.acc_g_vector[x] + usersettings.acccalibration[x];
+        global.acc_g_vector[x] = global.acc_g_vector[x] + usersettings.acccalibration[x];
 				acc[x]= global.acc_g_vector[x]>>6; 
     }
 
 // deadzone for yaw rate	
 //global.gyrorate[2] = ( 0 || global.gyrorate[2] > 40000 || global.gyrorate[2]< -40000 ) *  global.gyrorate[2] ;
-		
-#ifndef SMALL_ANGLE_APPROX	
-float gyros[3];
+
+
 for ( int i = 0 ; i < 3; i++)
 {		
 	gyros[i]  = tofloat( global.gyrorate[i] ) * deltatime * 0.01745329;
-	//gyros[1]  = tofloat( global.gyrorate[1] ) * deltatime * 0.01745329;
-	//gyros[2]  = tofloat( global.gyrorate[2] ) * deltatime * 0.01745329;
 }
-	
-    // This does a  "proper" matrix rotation using gyro deltas without small-angle approximation
-    float mat[3][3];
-    float cosx, sinx, cosy, siny, cosz, sinz;
-    float coszcosx, coszcosy, sinzcosx, coszsinx, sinzsinx;
+		
+#ifndef SMALL_ANGLE_APPROX	
+ // This does a  "proper" matrix rotation using gyro deltas without small-angle approximation
+	float mat[3][3];
+  float tempvect[3];
+	float cosx, sinx, cosy, siny, cosz, sinz;
+	float coszcosx, coszcosy, sinzcosx, coszsinx, sinzsinx;
+vectorcopy ( &tempvect[0] , & EstG[0] );
+
 // the signs are differnt due to different conventions
 // for positive/negative angles in various multiwii forks this is based on
-    cosx = _cosf( gyros[1]);
-    sinx = _sinf( gyros[1]);
-    cosy = _cosf( -gyros[0]);
-    siny = _sinf( -gyros[0]);
-    cosz = _cosf( -gyros[2]);
-    sinz = _sinf( -gyros[2]);
+	cosx = _cosf( gyros[1]);
+	sinx = _sinf( gyros[1]);
+	cosy = _cosf( -gyros[0]);
+	siny = _sinf( -gyros[0]);
+	cosz = _cosf( -gyros[2]);
+	sinz = _sinf( -gyros[2]);
 
-    coszcosx = cosz * cosx;
-    coszcosy = cosz * cosy;
-    sinzcosx = sinz * cosx;
-    coszsinx = sinx * cosz;
-    sinzsinx = sinx * sinz;
+	coszcosx = cosz * cosx;
+	coszcosy = cosz * cosy;
+	sinzcosx = sinz * cosx;
+	coszsinx = sinx * cosz;
+	sinzsinx = sinx * sinz;
 
-    mat[0][0] = coszcosy;
-    mat[0][1] = -cosy * sinz;
-    mat[0][2] = siny;
-    mat[1][0] = sinzcosx + (coszsinx * siny);
-    mat[1][1] = coszcosx - (sinzsinx * siny);
-    mat[1][2] = -sinx * cosy;
-    mat[2][0] = (sinzsinx) - (coszcosx * siny);
-    mat[2][1] = (coszsinx) + (sinzcosx * siny);
-    mat[2][2] = cosy * cosx;
+	mat[0][0] = coszcosy;
+	mat[0][1] = -cosy * sinz;
+	mat[0][2] = siny;
+	mat[1][0] = sinzcosx + (coszsinx * siny);
+	mat[1][1] = coszcosx - (sinzsinx * siny);
+	mat[1][2] = -sinx * cosy;
+	mat[2][0] = (sinzsinx) - (coszcosx * siny);
+	mat[2][1] = (coszsinx) + (sinzcosx * siny);
+	mat[2][2] = cosy * cosx;
 
-    EstG[0] = GEstG[0] * mat[0][0] + GEstG[1] * mat[1][0] + GEstG[2] * mat[2][0];
-    EstG[1] = GEstG[0] * mat[0][1] + GEstG[1] * mat[1][1] + GEstG[2] * mat[2][1];
-    EstG[2] = GEstG[0] * mat[0][2] + GEstG[1] * mat[1][2] + GEstG[2] * mat[2][2];
-//	*/
+	EstG[0] = tempvect[0] * mat[0][0] + tempvect[1] * mat[1][0] + tempvect[2] * mat[2][0];
+	EstG[1] = tempvect[0] * mat[0][1] + tempvect[1] * mat[1][1] + tempvect[2] * mat[2][1];
+	EstG[2] = tempvect[0] * mat[0][2] + tempvect[1] * mat[1][2] + tempvect[2] * mat[2][2];
+
 #endif // end rotation matrix
 
 #ifdef SMALL_ANGLE_APPROX
+// this is a rotation with small angle approximation
   float deltagyroangle;	 // holds float gyro angle in rad
 	// Rotate Estimated vector(s), ROLL
-  deltagyroangle  =  tofloat( global.gyrorate[0]) * deltatime * 0.01745329 ;
-  EstG[2] =  scos(deltagyroangle) * EstG[2] - ssin(deltagyroangle) * EstG[0];
-  EstG[0] =  ssin(deltagyroangle) * EstG[2] + scos(deltagyroangle) * EstG[0];
+  EstG[2] =  scos(gyros[0]) * EstG[2] - ssin(gyros[0]) * EstG[0];
+  EstG[0] =  ssin(gyros[0]) * EstG[2] + scos(gyros[0]) * EstG[0];
 		
   // Rotate Estimated vector(s), PITCH
-  deltagyroangle  = tofloat( global.gyrorate[1] )  * deltatime * 0.01745329;
-  EstG[1] =  scos(deltagyroangle) * EstG[1] + ssin(deltagyroangle) * EstG[2];
-  EstG[2] = -ssin(deltagyroangle) * EstG[1] + scos(deltagyroangle) * EstG[2];
+  EstG[1] =  scos(gyros[1]) * EstG[1] + ssin(gyros[1]) * EstG[2];
+  EstG[2] = -ssin(gyros[1]) * EstG[1] + scos(gyros[1]) * EstG[2];
 
   // Rotate Estimated vector(s), YAW
-  deltagyroangle  = tofloat( global.gyrorate[2] ) * deltatime * 0.01745329;
-  EstG[0] =  scos(deltagyroangle) * EstG[0] - ssin(deltagyroangle) * EstG[1];
-  EstG[1] =  ssin(deltagyroangle) * EstG[0] + scos(deltagyroangle) * EstG[1];
+  EstG[0] =  scos(gyros[2]) * EstG[0] - ssin(gyros[2]) * EstG[1];
+  EstG[1] =  ssin(gyros[2]) * EstG[0] + scos(gyros[2]) * EstG[1];
 
 #endif // end small angle approx
 
@@ -310,7 +289,7 @@ for ( int i = 0 ; i < 3; i++)
 
 // yaw without deadzone
 // not tested , i am not sure what the yaw angle is even used for
- global.currentestimatedeulerattitude[YAWINDEX] += ( (float) ( global.gyrorate[2] ) * deltatime + 0.5f);
+ global.currentestimatedeulerattitude[YAWINDEX] += ( (float) ( global.gyrorate[2] ) * deltatime);
 
    lib_fp_constrain180(&global.currentestimatedeulerattitude[YAWINDEX]);	
 
@@ -328,7 +307,7 @@ if (1)
 	{
 	 for (int axis = 0; axis < 3; axis++) 
 		{
-			EstG[axis] =  EstG[axis] / ( mag / normal );
+			EstG[axis] =  EstG[axis] / ( mag / ACC_1G );
 		}	
 	}
 //debug4 = mag;
@@ -344,7 +323,7 @@ accmag = calcmagnitude( &acc[0] );
 	// normalize acc
  for (int axis = 0; axis < 3; axis++) 
 	{
-	  acc[axis] =  acc[axis] / (accmag / normal) ;
+	  acc[axis] =  acc[axis] / (accmag / ACC_1G) ;
 	}	
 
 // test acc mag
